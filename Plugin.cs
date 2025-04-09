18e7dac4bb69cecad0ee61dd70a3f85ff81fbadf
Martin Kjeldsen <kjeldsen@gmail.com>
2025-04-08 22:05:11 -0400

    Initial

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace PantheonPlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"MyPlugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
