d1c99189f90f8acbe306e5df5b999d04886f9ee0
Martin Kjeldsen <kjeldsen@gmail.com>
2025-04-09 22:01:45 -0400

    build too

--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-scp bin/Debug/net6.0/PantheonPlugin.dll deck@192.168.86.42:'/run/media/mmcblk0p1/users/steamuser/Documents/My Games/Pantheon/App/BepInEx/plugins'
+dotnet build; scp bin/Debug/net6.0/PantheonPlugin.dll deck@192.168.86.42:'/run/media/mmcblk0p1/users/steamuser/Documents/My Games/Pantheon/App/BepInEx/plugins'
