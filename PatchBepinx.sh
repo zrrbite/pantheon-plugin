490258063db03266573cc00c9de7bff319454cdd
Martin Kjeldsen <kjeldsen@gmail.com>
2025-04-25 17:33:08 -0400

    Patch process

---
+++ PatchBepinx.sh
@@ -0,0 +1,6 @@
+cd '/run/media/mmcblk0p1/users/steamuser/Documents/My Games/Pantheon/App/'
+wget 'https://builds.bepinex.dev/projects/bepinex_be/733/BepInEx-Unity.IL2CPP-win-x64-6.0.0-be.733%2B995f049.zip'
+unzip BepInEx-Unity.IL2CPP-win-x64-6.0.0-be.733+995f049.zip
+# launch Panthon and see it patch. Close game.
+# upload plugin to /Bepinex/plugins folder
+# launch again
