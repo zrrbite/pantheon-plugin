0af5e77cbea016b6eb8e0e8eaebfbbf460453d98
Martin Kjeldsen <kjeldsen@gmail.com>
2025-07-03 22:31:58 -0400

    dl interop clean

--- dl_interop.sh
+++ dl_interop.sh
@@ -1,1 +1,5 @@
-scp -r deck@192.168.86.42:'/run/media/mmcblk0p1/users/steamuser/Documents/My Games/Pantheon/App/BepInEx/interop/' ../interop_04_25_25
+rm -r assemblies_old
+mv assemblies/ assemblies_old/
+rm -r assemblies
+scp -r deck@192.168.86.42:'/run/media/mmcblk0p1/users/steamuser/Documents/My Games/Pantheon/App/BepInEx/interop/' assemblies
+
